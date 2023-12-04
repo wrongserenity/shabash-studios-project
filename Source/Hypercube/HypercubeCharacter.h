589188a268697add3204d566dad44277f4ee0109
Karkusha23 <min.rinat567321@gmail.com>
2023-12-04 17:43:19 +0300

    Fix player animation

--- Source/Hypercube/HypercubeCharacter.h
+++ Source/Hypercube/HypercubeCharacter.h
@@ -183,13 +183,16 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
 	float CameraFovChangeSpeed;
 
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EPlayerMovementPhase MovementPhase;
+
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EPlayerAttackPhase AttackPhase;
+
 protected:
 
 	class UCharacterMovementComponent* MoveComp;
 	class UInputComponent* InputComp;
-
-	EPlayerMovementPhase MovementPhase;
-	EPlayerAttackPhase AttackPhase;
 
 	bool bCanDash;
 	bool bDashMovementBlocked;
